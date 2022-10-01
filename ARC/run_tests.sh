f353cd36fc663da896b510788216657b8b86d400
Danny <bublikplushka@yandex.ru>
2022-10-02 02:26:37 +0300

    updated test names, regenerate tests

---
+++ ARC/run_tests.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+for dir in TESTS/*; do
+    if [ -d "$dir" ]; then
+        full_path="$dir/test"
+        ans=$(cat $full_path | build/ARC/arc_cache)
+        echo "Test $full_path: total hits: $ans"
+    fi
+done
