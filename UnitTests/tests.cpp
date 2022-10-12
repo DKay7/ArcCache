e9c86141a4e2dd28fa7a945eea703cce6d6212d6
Danny <bublikplushka@yandex.ru>
2022-10-13 00:32:40 +0300

    Update unit tests for perfect cache

--- UnitTests/tests.cpp
+++ UnitTests/tests.cpp
@@ -35,35 +35,52 @@ TEST(ArcPushTest, HandleKeyTNotEqPageT)
 
 }
 
-TEST(PerfectCachePushTest, HandleKeyTNotEqPageT)
-{       
+TEST(PerfectCachePushTest, test_1) {
+    std::vector<int> pages = {1, 2, 3, 4, 1, 2, 5, 1, 2, 4, 3, 4};
 
-    SomePage page1{11, "str1"};
-    SomePage page2{12, "str2"};
-    SomePage page3{13, "str3"};
-    SomePage page4{14, "str4"};
-    SomePage page5{15, "str5"};
+    perfect_cache::data_t<int, int> data = perfect_cache::get_vec_of_pairs(pages, pages);
+    EXPECT_EQ(7, perfect_cache::count_hits(4, data));
+}
 
-    std::vector<std::string> pages{page1.data, page2.data, page3.data, page4.data, page5.data, page1.data, page2.data, page3.data};
-    cache::PerfectCache<int, std::string> perfect_cache{3, pages};
-    
+TEST(PerfectCachePushTest, test_2) {
+    std::vector<int> pages = {1, 2, 99, 3, 4, 5, 112, 6, 1, 0, 2, 7, 8};
 
+    perfect_cache::data_t<int, int> data = perfect_cache::get_vec_of_pairs(pages, pages);
+    EXPECT_EQ(2, perfect_cache::count_hits(5, data));
+}
 
-    EXPECT_EQ(perfect_cache.push(page1.id, page1.data), 0);
-    EXPECT_EQ(perfect_cache.push(page2.id, page2.data), 0);
-    EXPECT_EQ(perfect_cache.push(page3.id, page3.data), 0);
-    EXPECT_EQ(perfect_cache.push(page4.id, page4.data), 0);
-    EXPECT_EQ(perfect_cache.push(page5.id, page5.data), 0);
-    EXPECT_EQ(perfect_cache.push(page1.id, page1.data), 1);
-    EXPECT_EQ(perfect_cache.push(page2.id, page2.data), 1);
-    EXPECT_EQ(perfect_cache.push(page3.id, page3.data), 0);
+TEST(PerfectCachePushTest, test_3) {
+    std::vector<int> pages = {2, 4, 2, 4, 6, 4, 6, 7, 6, 6, 7, 9, 6, 4, 3, 5, 7, 8, 6, 5, 4, 3, 4, 5, 7, 8, 8, 7, 6};
 
-
-    EXPECT_EQ(perfect_cache.lookup(page1.id), 1);
-    EXPECT_EQ(perfect_cache.lookup(page2.id), 1);
-    EXPECT_EQ(perfect_cache.lookup(page3.id), 1);
-    EXPECT_EQ(perfect_cache.lookup(page4.id), 0);
-    EXPECT_EQ(perfect_cache.lookup(page5.id), 0);
+    perfect_cache::data_t<int, int> data = perfect_cache::get_vec_of_pairs(pages, pages);
+    EXPECT_EQ(20, perfect_cache::count_hits(5, data));
+}
+ 
+TEST(PerfectCachePushTest, test_4) {
+    std::vector<int> pages = {91, 123, 1, 2, 3, 4, 5, 1, 2, 3, 4, 5};
 
 
+    perfect_cache::data_t<int, int> data = perfect_cache::get_vec_of_pairs(pages, pages);
+    EXPECT_EQ(5, perfect_cache::count_hits(5, data));
+}
+
+TEST(PerfectCachePushTest, test_5) {
+    std::vector<int> pages = {7, 2, 7, 4, 7, 8, 7, 16, 7, 32, 7, 64, 7, 128, 7, 256, 7, 1024, 7};
+
+    perfect_cache::data_t<int, int> data = perfect_cache::get_vec_of_pairs(pages, pages);
+    EXPECT_EQ(9, perfect_cache::count_hits(2, data));
+}
+
+TEST(PerfectCachePushTest, test_6) {
+    std::vector<int> pages = {4, 7, 3, 6, 85, 34, 3276, 6, 8, 5, 3, 4242, 4, 7, 8, 6, 4, 4, 5, 6, 7, 8};
+
+    perfect_cache::data_t<int, int> data = perfect_cache::get_vec_of_pairs(pages, pages);
+    EXPECT_EQ(11, perfect_cache::count_hits(5, data));
+}
+
+TEST(PerfectCachePushTest, test_7) {
+    std::vector<int> pages = {2, 2, 1, 3, 3, 73, 16, 4, 1, 5, 6, 2};
+    perfect_cache::data_t<int, int> data = perfect_cache::get_vec_of_pairs(pages, pages);
+
+    EXPECT_EQ(4, perfect_cache::count_hits(3, data));
 }
